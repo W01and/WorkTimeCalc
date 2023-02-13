6a208fd8734e43711a83a0ba7d4d4a9059e9d4f6
Maxim <wolandmax@ya.ru>
2023-02-13 22:37:26 +0300

    Чтение рабочего времени из файла

--- src/main.go
+++ src/main.go
@@ -13,6 +13,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -26,21 +27,50 @@ func main() {
 	}
 
 	// Структура для хранения данных
-	var data, time1, time2 []string
-	var i int = 0
-	// Создайте читателя, связанного с файлом
+	var time1Hour, time1Min int
+	var time2Hour, time2Min int
+	//	var data, time1, time2 []string
+	var ost int = 0 // остаток
+	// Создаём читателя, связанного с файлом
+	obed := 45
+	stand := 390
 	reader := bufio.NewReader(file)
 	for {
 		// Читаем строку из файла
 		line, err := reader.ReadString('\n')
 		// Уберем последний символ перевода строки
 		line = strings.TrimSuffix(line, "\n")
-		// Разделите строку на части
+		line = strings.TrimSuffix(line, "\r")
+		fmt.Println("Считанная строка: ", line)
+		// Разделим строку на части
 		parts := strings.Split(line, " ")
-		// Запишите данные в переменные
-		data = append(data, parts[0])
-		time1 = append(time1, parts[1])
-		time2 = append(time2, parts[2])
+		// Запишем данные в переменные
+		if len(parts) == 3 {
+			time1x := strings.Split(parts[1], ":")
+			time1Hour, err = strconv.Atoi(time1x[0])
+			if err != nil {
+				fmt.Println("ошибка", err)
+			}
+			time1Min, err = strconv.Atoi(time1x[1])
+			if err != nil {
+				fmt.Println("ошибка", err)
+			}
+			fmt.Println("time1Hour = ", time1Hour, "\n time1Min = ", time1Min)
+
+			time2x := strings.Split(parts[2], ":")
+			fmt.Println("time2x = ", time2x)
+			time2Hour, err = strconv.Atoi(time2x[0])
+			if err != nil {
+				fmt.Println("ошибка", err)
+			}
+			time2Min, err = strconv.Atoi(time2x[1])
+			if err != nil {
+				fmt.Println("ошибка", err)
+			}
+			fmt.Println("time2Hour = ", time2Hour, "\n time2Min = ", time2Min)
+			ost += (time2Hour*60 + time2Min) - (time1Hour*60 + time1Min) - obed - stand
+			fmt.Println("ost = ", ost)
+		}
 		if err != nil {
 			if err == io.EOF {
 				break
@@ -48,15 +78,8 @@ func main() {
 			fmt.Println("ошибка", err)
 			return
 		}
-		i++
 	}
-	// ost - остаток
-	//	ost:=0
-	for j := 0; j <= i; j++ {
-
-		fmt.Printf("Data: %s, time1: %s, time2: %s\n", data[j], time1[j], time2[j])
-	}
-
+	fmt.Println("ost = ", ost)
 	defer file.Close()
 
 }
